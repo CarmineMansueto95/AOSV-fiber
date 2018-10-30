0328445cf4b29a67e68eb00ddbc6eb4a56735e8c
CarmineMansueto95 <carminemansueto95@gmail.com>
2018-10-30 17:18:29 +0100

    Started to work on ioctl

--- fiber_module.c
+++ fiber_module.c
@@ -1,10 +1,26 @@
 #include <linux/module.h>
 #include <linux/kernel.h>	//needed for printk() priorities
+#include "headers/fiber_module.h"	//for macros and function declarations
 
 #define AUTHOR "Carmine Mansueto"
 
 static struct cdev* fib_cdev;
 static dev_t fib_cdevt;			//for the return of alloc_chrdev_region. This will contain the MAJOR number and the first MINOR number (16 bit for each).
+
+//file operations for the device
+const struct file_operations fops = {
+	.open = my_open;
+	.unlocked_ioctl = ioctl_commands; //function that handles the ioctl calls
+}
+
+long ioctl_commands(struct file* filp, unsigned int cmd, unsigned long arg){
+	switch(cmd){
+		case IOCTL_TEST:
+			printk(KERN_INFO "ioctl issued with IOCTL_TEST command!\n");
+	}
+	
+	return 0;
+}
 
 static int __init mod_init(void){
 	
@@ -12,10 +28,11 @@ static int __init mod_init(void){
 	
 	int ret;
 	
-	//Dinamically allocating device MAJOR number and first MINOR number
+	//Allocating device MAJOR number and first MINOR number
+	//The first parameter will contain, after the execution, the MAJOR and the first MINOR
 	ret = alloc_chrdev_region(&fib_cdevt, 1, NUM_MINORS, DEVICE_NAME);
 	if(ret!=0){
-		printk(KERN_INFO "Could not allocate device!\n");
+		printk(KERN_INFO "Could not allocate MAJOR and MINOR!\n");
 		return -EFAULT;
 	}
 	
@@ -26,7 +43,7 @@ static int __init mod_init(void){
 		return -EFAULT;
 	}
 	
-	//Initialize the struct cdev with fops (fops still to be done)
+	//Initialize the struct cdev with fops, such that from now on the devices has some associated operations
 	cdev_init(fib_cdev, &fops);
 	
 	//Registering device into the system
